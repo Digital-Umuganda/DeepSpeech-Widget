3ff35b6f6fd02a2a76a1ca183cb33752fbb9765b
lcdamy <zudanga@gmail.com>
2021-04-15 14:08:39 +0200

    before i add authantication fx

--- js/app.js
+++ js/app.js
@@ -1,129 +1,55 @@
-//webkitURL is deprecated but nevertheless
 URL = window.URL || window.webkitURL;
-
 var c = 0;
 var t;
-var timer_is_on = 0
-
-var gumStream; 						
-var rec; 						
+var timer_is_on = 0;
+var gumStream;
+var rec;
 var input;
-
-var globalBlob;//MediaStreamAudioSourceNode we'll be recording
-
-// shim for AudioContext when it's not avb. 
+var globalBlob;
 var AudioContext = window.AudioContext || window.webkitAudioContext;
-var audioContext //audio context to help us record
+var audioContext;
+var filename;
 
 var recordButton = document.getElementById("recordButton");
 var stopButton = document.getElementById("stopButton");
 var displayContent = document.getElementById("displayContent");
-var musicFile = document.getElementById("musicFileS");
 var audioLink = document.getElementById("audioLink");
-// var pauseButton = document.getElementById("pauseButton");
+var Transilate = document.getElementById('Transilate');
+var transilatedAudio = document.getElementById('transilatedAudio');
 
-//add events to those 2 buttons
+Transilate.addEventListener("click", transilate);
+
 recordButton.addEventListener("click", startRecording);
 stopButton.addEventListener("click", stopRecording);
-// pauseButton.addEventListener("click", pauseRecording);
 
 function startRecording() {
-	console.log("recordButton clicked");
 	c = 0;
 	timer_is_on = 0;
-	/*
-		Simple constraints object, for more advanced audio features see
-		https://addpipe.com/blog/audio-constraints-getusermedia/
-	*/
-
-	var constraints = { audio: true, video: false }
-
-	/*
-	  Disable the record button until we get a success or fail from getUserMedia() 
-  */
-
+	var constraints = { audio: true, video: false };
 	recordButton.disabled = true;
 	stopButton.disabled = false;
-	// pauseButton.disabled = false
-
-	/*
-		We're using the standard promise based getUserMedia() 
-		https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
-	*/
-
 	navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
 		console.log("getUserMedia() success, stream created, initializing Recorder.js ...");
-
-		/*
-			create an audio context after getUserMedia is called
-			sampleRate might change after getUserMedia is called, like it does on macOS when recording through AirPods
-			the sampleRate defaults to the one set in your OS for your playback device
-
-		*/
 		audioContext = new AudioContext();
-
-		//update the format 
 		startCount();
-
-
-		/*  assign to gumStream for later use  */
 		gumStream = stream;
-
-		/* use the stream */
 		input = audioContext.createMediaStreamSource(stream);
-
-		/* 
-			Create the Recorder object and configure to record mono sound (1 channel)
-			Recording 2 channels  will double the file size
-		*/
-		rec = new Recorder(input, { numChannels: 1 })
-
-		//start the recording process
-		rec.record()
-
+		rec = new Recorder(input, { numChannels: 1 });
+		rec.record();
 		console.log("Recording started");
-
 	}).catch(function (err) {
-		//enable the record button if getUserMedia() fails
 		recordButton.disabled = false;
 		stopButton.disabled = true;
-		// pauseButton.disabled = true
 	});
 }
 
-function pauseRecording() {
-	console.log("pauseButton clicked rec.recording=", rec.recording);
-	if (rec.recording) {
-		//pause
-		rec.stop();
-		pauseButton.innerHTML = "Resume";
-	} else {
-		//resume
-		rec.record()
-		pauseButton.innerHTML = "Pause";
-
-	}
-}
-
 function stopRecording() {
-	console.log("stopButton clicked");
 	displayContent.style.visibility = "visible";
 	stopCount();
-	//disable the stop button, enable the record too allow for new recordings
 	stopButton.disabled = true;
 	recordButton.disabled = false;
-	// pauseButton.disabled = true;
-
-	//reset button just in case the recording is stopped while paused
-	// pauseButton.innerHTML = "Pause";
-
-	//tell the recorder to stop the recording
 	rec.stop();
-
-	//stop microphone access
 	gumStream.getAudioTracks()[0].stop();
-
-	//create the wav blob and pass it on to createDownloadLink
 	rec.exportWAV(createDownloadLink);
 }
 
@@ -131,61 +57,14 @@ function createDownloadLink(blob) {
 	globalBlob = blob;
 	var url = URL.createObjectURL(blob);
 	var au = document.createElement('audio');
-	var li = document.createElement('li');
-	var link = document.createElement('a');
-
-	//name of .wav file to use during upload and download (without extendion)
-	var filename = new Date().toISOString();
-
-	//add controls to the <audio> element
+	filename = new Date().toISOString();
 	au.controls = true;
 	au.src = url;
-
-	//save to disk link
-	link.href = url;
-	link.download = filename + ".wav"; //download forces the browser to donwload the file using the  filename
-	link.innerHTML = "Save";
-
-	//add the new audio element to li
-	li.appendChild(au);
-
-	//add the filename to the li
-	//li.appendChild(document.createTextNode(filename + ".wav "))
-
-	//add the save to disk link to li
-	//li.appendChild(link);
 	audioLink.href = url;
 	audioLink.download = filename + ".wav ";
-	audioLink.innerHTML = filename + ".wav ";
-	musicFileS.src = filename + ".wav ";
-
-	//upload link
-	// var upload = document.createElement('button');
-	// upload.type = "button";
-	// upload.classList.add("btn");
-	// upload.classList.add("btn-primary");
-	// upload.classList.add("btn-sm");
-	// upload.innerHTML = "Upload";
-	// upload.addEventListener("click", function (event) {
-	// 	var xhr = new XMLHttpRequest();
-	// 	xhr.onload = function (e) {
-	// 		if (this.readyState === 4) {
-	// 			console.log("Server returned: ", e.target.responseText);
-	// 		}
-	// 	};
-	// 	var fd = new FormData();
-	// 	fd.append("audio_data", blob, filename);
-	// 	xhr.open("POST", "upload.php", true);
-	// 	xhr.send(fd);
-	// })
-	li.appendChild(document.createTextNode(" "))//add a space in between
-	//li.appendChild(upload)//add the upload link to li
-
-	//add the li element to the ol
-	recordingsList.appendChild(li);
+	audioLink.innerHTML = "Listen to your record";
+	recordedArea.appendChild(au);
 }
-
-
 
 function timedCount() {
 	document.getElementById("formats").innerHTML = "Format: 1 channel pcm @ " + audioContext.sampleRate / 1000 + "kHz | <span class='red'><b>" + c + "</b> sec </span>";
@@ -205,111 +84,6 @@ function stopCount() {
 	timer_is_on = 0;
 }
 
-
-
-
-
-
-
-var music = document.getElementById('music'); // id for audio element
-var duration = music.duration; // Duration of audio clip, calculated here for embedding purposes
-var pButton = document.getElementById('pButton'); // play button
-var playhead = document.getElementById('playhead'); // playhead
-var timeline = document.getElementById('timeline'); // timeline
-var Transilate = document.getElementById('Transilate'); // timeline
-var transilatedAudio = document.getElementById('transilatedAudio'); // timeline
-
-// timeline width adjusted for playhead
-var timelineWidth = timeline.offsetWidth - playhead.offsetWidth;
-
-// play button event listenter
-pButton.addEventListener("click", play);
-
-Transilate.addEventListener("click", transilate);
-
-// timeupdate event listener
-music.addEventListener("timeupdate", timeUpdate, false);
-
-// makes timeline clickable
-timeline.addEventListener("click", function (event) {
-	moveplayhead(event);
-	music.currentTime = duration * clickPercent(event);
-}, false);
-
-// returns click as decimal (.77) of the total timelineWidth
-function clickPercent(event) {
-	return (event.clientX - getPosition(timeline)) / timelineWidth;
-}
-
-// makes playhead draggable
-playhead.addEventListener('mousedown', mouseDown, false);
-window.addEventListener('mouseup', mouseUp, false);
-
-// Boolean value so that audio position is updated only when the playhead is released
-var onplayhead = false;
-
-// mouseDown EventListener
-function mouseDown() {
-	onplayhead = true;
-	window.addEventListener('mousemove', moveplayhead, true);
-	music.removeEventListener('timeupdate', timeUpdate, false);
-}
-
-// mouseUp EventListener
-// getting input from all mouse clicks
-function mouseUp(event) {
-	if (onplayhead == true) {
-		moveplayhead(event);
-		window.removeEventListener('mousemove', moveplayhead, true);
-		// change current time
-		music.currentTime = duration * clickPercent(event);
-		music.addEventListener('timeupdate', timeUpdate, false);
-	}
-	onplayhead = false;
-}
-// mousemove EventListener
-// Moves playhead as user drags
-function moveplayhead(event) {
-	var newMargLeft = event.clientX - getPosition(timeline);
-
-	if (newMargLeft >= 0 && newMargLeft <= timelineWidth) {
-		playhead.style.marginLeft = newMargLeft + "px";
-	}
-	if (newMargLeft < 0) {
-		playhead.style.marginLeft = "0px";
-	}
-	if (newMargLeft > timelineWidth) {
-		playhead.style.marginLeft = timelineWidth + "px";
-	}
-}
-
-// timeUpdate
-// Synchronizes playhead position with current point in audio
-function timeUpdate() {
-	var playPercent = timelineWidth * (music.currentTime / duration);
-	playhead.style.marginLeft = playPercent + "px";
-	if (music.currentTime == duration) {
-		pButton.className = "";
-		pButton.className = "fas fa-play";
-	}
-}
-
-//Play and Pause
-function play() {
-	// start music
-	if (music.paused) {
-		music.play();
-		// remove play, add pause
-		pButton.className = "";
-		pButton.className = "fas fa-pause";
-	} else { // pause music
-		music.pause();
-		// remove pause, add play
-		pButton.className = "";
-		pButton.className = "fas fa-play";
-	}
-}
-
 function transilate() {
 	transilatedAudio.style.visibility = "visible";
 	var xhr = new XMLHttpRequest();
@@ -319,12 +93,7 @@ function transilate() {
 		}
 	};
 	var fd = new FormData();
-	fd.append("audio_data", globalBlob, filename);
-	xhr.open("POST", "upload.php", true);
+	fd.append("audio", globalBlob, filename);
+	xhr.open("POST", "https://mbaza.dev.cndp.org.rw/deepspeech/api/api/stt/http", true);
 	xhr.send(fd);
 }
-
-// Gets audio file duration
-music.addEventListener("canplaythrough", function () {
-	duration = music.duration;
-}, false);
